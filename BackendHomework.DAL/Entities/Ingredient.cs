0f88e2aabcb4796d7c4144f3bd47bfd65718bb1d
Jhon <jhond.b.v@gmail.com>
2021-11-13 13:30:23 -0500

    clean architecture implementation

---
+++ BackendHomework.DAL/Entities/Ingredient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackendHomework.Core.Models
+{
+    public class Ingredient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int PlateId { get; set; }
+        public Plate Plate { get; set; }
+    }
+}
---
+++ BackendHomework.DAL/Entities/Plate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackendHomework.Core.Models
+{
+    public class Plate
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+        ICollection<Ingredient> Ingredients { get; set; }
+
+    }
+}
