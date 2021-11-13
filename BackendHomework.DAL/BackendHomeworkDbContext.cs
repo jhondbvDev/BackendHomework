1ff435a1e58fc2fd9350206c0951e351fa453c17
jhondbvDev <Jjhond.b.v@hotmail.com>
2021-11-13 11:55:50 -0500

    Agregar archivos de proyecto.

---
+++ BackendHomework.DAL/BackendHomeworkDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendHomework.DAL
+{
+    public class BackendHomeworkDbContext : IdentityDbContext<IdentityUser>
+    {
+        public BackendHomeworkDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
