1ddecda2b966ddd47eca1fa55c68d195f13cb3be
Jhon <jhond.b.v@gmail.com>
2021-11-14 12:59:39 -0500

    RandomAPI implementation

---
+++ BackendHomework.API/Controllers/RandomAPIController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BackendHomework.API.Response;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace BackendHomework.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RandomAPIController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("randomnumber")]
+        public async Task<IActionResult> GetRandomNumber()
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync("https://csrng.net/csrng/csrng.php?min=0&max=1000");
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string message =  response.Content.ReadAsStringAsync().Result;
+
+                        ResponseRandomAPI responseRandom = JsonConvert.DeserializeObject<List<ResponseRandomAPI>>(message).First();
+                        return Ok(new ResponseMessage<Object>(new { RandomNumber=responseRandom.Random }));
+                    }
+                    else
+                    {
+                        return BadRequest(new ResponseMessage<string>("The service is temporaly out of services, try later."));
+                    }
+
+                  
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResponseMessage<Exception>(ex));
+            }
+        }
+    }
+}
---
+++ BackendHomework.API/Response/ResponseRandomAPI.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendHomework.API.Response
+{
+    public class ResponseRandomAPI
+    {
+        [JsonProperty("status")]
+        public string Status { get; set; }
+        [JsonProperty("min")]
+        public int Min { get; set; }
+        [JsonProperty("max")]
+        public int Max { get; set; }
+        [JsonProperty("random")]
+        public int Random { get; set; }
+    }
+}
